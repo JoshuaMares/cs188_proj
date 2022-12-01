00bdc81a2c58816b846d0c2b6056a437da05e684
Josh <joshuamares180@gmail.com>
2022-12-01 14:43:35 -0800

    fixed outputdir

--- scripts/test_com2sense_deberta.sh
+++ scripts/test_com2sense_deberta.sh
@@ -1,16 +1,18 @@
+TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
+MODEL_TYPE="microsoft/deberta-base"
 #MODEL_TYPE="bert-base-cased"
 #MODEL_TYPE="roberta-base"
 MODEL_TYPE="microsoft/deberta-base"
 TRAINED_MODEL="outputs/com2sense/ckpts/checkpoint-5000"
-TASK_NAME="com2sense"
+
 OUTPUT_DIR=${TASK_NAME}/ckpts
 
 
 CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
   --config_name "${MODEL_TYPE}" \
-  --output_dir "${OUTPUT_DIR}/ckpts" \
+  --output_dir "${OUTPUT_DIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --eval_split "test" \
