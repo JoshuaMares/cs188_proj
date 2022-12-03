1c4fac4aaaf437aaa14bb146c4a7e07bea01d5f3
Josh <joshuamares180@gmail.com>
2022-12-02 21:25:05 -0800

    changed from 5k iterations to 1 epoch

--- scripts/train_semeval_deberta.sh
+++ scripts/train_semeval_deberta.sh
@@ -13,12 +13,12 @@ CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --per_gpu_train_batch_size 4 \
   --per_gpu_eval_batch_size 1 \
   --learning_rate 1e-5 \
-  --max_steps 5000 \
+  --num_train_epochs 1 \
   --max_seq_length 128 \
   --output_dir "${OUTPUT_DIR}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
-  --save_steps 1000 \
+  --save_steps 5000 \
   --logging_steps 1000 \
   --warmup_steps 100 \
   --eval_split "dev" \
