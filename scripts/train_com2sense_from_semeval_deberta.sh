f5de871461385ab64e94c3902cf53e9163b9e49f
Josh <joshuamares180@gmail.com>
2022-12-03 18:05:00 -0800

    training from sem2eval 15k for com2sense

--- scripts/train_com2sense_from_semeval_deberta.sh
+++ scripts/train_com2sense_from_semeval_deberta.sh
@@ -3,27 +3,28 @@ DATA_DIR="datasets/com2sense"
 #MODEL_TYPE="roberta-base"
 MODEL_TYPE="microsoft/deberta-base"
 TASK_NAME="com2sense"
-OUTPUT_DIR=${TASK_NAME}/ckpts
-
+OUTPUT_DIR=${TASK_NAME}
 
 CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
-  --model_name_or_path ${MODEL_TYPE} \
+  --model_name_or_path "outputs/semeval/ckpts/checkpoint-15000" \
+  --tokenizer_name ${MODEL_TYPE} \
+  --config_name ${MODEL_TYPE} \
   --do_train \
   --do_eval \
   --eval_all_checkpoints \
-  --gradient_accumulation_steps 4 \
   --per_gpu_train_batch_size 4 \
   --per_gpu_eval_batch_size 1 \
   --learning_rate 1e-5 \
-  --max_steps 4000 \
+  --max_steps 1600 \
   --max_seq_length 128 \
-  --output_dir "${OUTPUT_DIR}" \
+  --output_dir "${OUTPUT_DIR}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
-  --save_steps 1000 \
-  --logging_steps 1000 \
+  --save_steps 300 \
+  --logging_steps 100 \
   --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "micro" \
   --do_not_load_optimizer \
-  #--overwrite_output_dir \
+  --evaluate_during_training \
+  --overwrite_output_dir \
