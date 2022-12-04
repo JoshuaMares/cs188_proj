86434a82acea21a891da2f1a1ad035cc49977e77
Josh <joshuamares180@gmail.com>
2022-12-04 14:34:58 -0800

    trying different learning rates and batch sizes

---
+++ scripts/looping_tc2sfs_deberta.sh
@@ -0,0 +1,37 @@
+DATA_DIR="datasets/com2sense"
+#MODEL_TYPE="bert-base-cased"
+#MODEL_TYPE="roberta-base"
+MODEL_TYPE="microsoft/deberta-base"
+TASK_NAME="com2sense"
+OUTPUT_DIR=${TASK_NAME}
+
+for gpu_train_batch_size in [4 8 16 32]
+do
+  for lr in [1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9]
+  do
+    echo
+    CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
+      --model_name_or_path "outputs/semeval/ckpts/checkpoint-45000" \
+      --tokenizer_name ${MODEL_TYPE} \
+      --config_name ${MODEL_TYPE} \
+      --do_train \
+      --do_eval \
+      --eval_all_checkpoints \
+      --per_gpu_train_batch_size $gpu_train_batch_size \
+      --per_gpu_eval_batch_size 1 \
+      --learning_rate $lr \
+      --max_steps 5000 \
+      --max_seq_length 128 \
+      --output_dir "${OUTPUT_DIR}/ckpts" \
+      --task_name "${TASK_NAME}" \
+      --data_dir "${DATA_DIR}" \
+      --save_steps 5000 \
+      --logging_steps 5000 \
+      --warmup_steps 100 \
+      --eval_split "dev" \
+      --score_average_method "micro" \
+      --do_not_load_optimizer \
+      --evaluate_during_training \
+      --overwrite_output_dir
+  done
+done
