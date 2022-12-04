5973c65c6fa528db1e690b52e62b9c55b820c8be
Josh <joshuamares180@gmail.com>
2022-12-03 21:14:17 -0800

    12 epochs to go through data 3 times

--- scripts/train_semeval_deberta.sh
+++ scripts/train_semeval_deberta.sh
@@ -13,13 +13,13 @@ CUDA_VISIBLE_DEVICES=0 python3 -m trainers.train \
   --per_gpu_train_batch_size 4 \
   --per_gpu_eval_batch_size 1 \
   --learning_rate 1e-5 \
-  --num_train_epochs 5 \
+  --num_train_epochs 12 \
   --max_seq_length 128 \
   --output_dir "${OUTPUT_DIR}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
-  --save_steps 500 \
-  --logging_steps 100 \
+  --save_steps 5000 \
+  --logging_steps 1000 \
   --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "binary" \
